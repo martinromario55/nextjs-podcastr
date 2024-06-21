d0d2ea39d397ad9566cc65daaf4555986addca29
martinromario55 <martinromario55@gmail.com>
2024-06-21 21:34:30 +0300

    Home Layout

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const CreatePodcastPage = () => {
-  return <div>CreatePodcastPage</div>;
+  return (
+    <div>
+      <h1 className="text-20 font-bold text-white-1">Create Podcasts</h1>
+    </div>
+  );
 };
 
 export default CreatePodcastPage;

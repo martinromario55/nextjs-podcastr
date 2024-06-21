aad687235807cd58b6ce69d6ff57d705e11bab41
martinromario55 <martinromario55@gmail.com>
2024-06-21 22:39:14 +0300

    Authentication and Database setup with Convex and Clerk

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,23 @@
+import Image from "next/image";
+
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
-  return <main>{children}</main>;
+  return (
+    <main className="relative h-screen w-full">
+      <div className="absolute size-full">
+        <Image
+          src={"/images/bg-img.png"}
+          alt="background"
+          fill
+          className="size-full"
+        />
+      </div>
+      {children}
+    </main>
+  );
 };
 
 export default AuthLayout;
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://alert-seal-88.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
